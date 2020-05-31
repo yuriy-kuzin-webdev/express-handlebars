06054848b667f40924ac8e0cce73608fec9a0ae7
yuriykuzinpd <yuriy.kuzin.pd@gmail.com>
2020-05-31 20:29:38 +0300

    products view

--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,16 @@
 const {Router}  =   require('express');
+const Product   =   require('../models/product');
 const router    =   Router();
 
 
-router.get('/', (req,res)=>{
+router.get('/', async (req,res)=>{
+
+    const products = await Product.getAll();
+
     res.render('products', {
         title       :   'Товары',
-        isProducts  :   true
+        isProducts  :   true,
+        products
     })
 });
 
